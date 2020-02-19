320a819c0a271a22ad05ac512530d74a550fda12
John De La Rosa <john.delarosa@ideapublicschools.org>
2020-02-19 00:28:23 -0600

    finish app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
+<div class="bg-green-500 min-h-screen">
+  <NavBar />
   <router-view />
+</div>
 </template>
+
+<script>
+import NavBar from './components/NavBar'
+
+export default {
+  name:'App',
+  components: {
+    NavBar
+  }
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-purple-700 p-6 h-20">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <router-link to="/" class="font-semibold text-xl tracking-tight"
+        >Post_It</router-link
+      >
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
